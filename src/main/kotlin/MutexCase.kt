d24298d4471a527f638d14e9a71d7991e71f253e
Dima Loktev <muakacho+github@gmail.com>
2018-09-27 16:14:04 +0300

    Update MutexCase.kt

--- src/main/kotlin/MutexCase.kt
+++ src/main/kotlin/MutexCase.kt
@@ -39,7 +39,7 @@ suspend fun expensiveAsyncIO(key: String): Int {
     console.log("${key} - expensiveAsyncIO started")
     delay(300)
     if (key == "key3") {
-        console.log("circular dependency")
+        console.log("an operation under a mutex calls another")
         withSomeMutex("key1")
     }
     console.log("${key} - expensiveAsyncIO for finished")
